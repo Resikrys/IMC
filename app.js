82cb47ca5127982a7d86a8055d30fb67174a7b65
resikrys <resicris@gmail.com>
2025-02-13 17:44:04 +0100

    card reveal event on work

--- app.js
+++ app.js
@@ -2,32 +2,40 @@
 // document.getElementById('calcular').addEventListener("click", calcularIMC());
 
 function calcularIMC() {
-    let peso = document.getElementById('inputWeight').value;
-    let altura = document.getElementById('inputHeight').value;
+    let peso = parseFloat(document.getElementById('inputWeight').value);
+    let altura = parseFloat(document.getElementById('inputHeight').value);
+
     let imc = peso / ((altura/100) * (altura/100)); //Altura/100 para pasar de cm a metros cuadrados (imc = peso (kg) / altura al cuadrado (m))
-    let resultado = '';
+    let mensaje = '';
+    let resultadoId = '';
 
     if (imc < 18) {
-        resultado = imc + ' Vaya, peso bajo';
-    } else if (imc < 25) {
-        resultado = imc + ' Peso normal, buen trabajo!';
-    } else if (imc < 30) {
-        resultado = imc + ' Sobrepeso';
+        mensaje = 'Vaya, peso bajo';
+        resultadoId = 'infoBajo';
+    } else if (imc >= 18.5 && imc < 24.9) {
+        mensaje = 'Peso normal, buen trabajo!';
+        resultadoId = 'infoBien';
+    } else if (imc >= 25 && imc < 29.9) {
+        mensaje = 'Sobrepeso';
+        resultadoId = imc + 'infoSobrepeso';
     } else if (imc > 30) {
-        resultado = imc + ' Obesidad';
+        mensaje = 'Obesidad';
+        resultadoId = 'infoObesidad';
     } else {
-        resultado = ' oops... introduce tu peso y altura y calcula tu IMC!';
+        mensaje = ' oops... introduce tu peso y altura y calcula tu IMC!';
     };
 
-    alert("Este es tu IMC: " + '' + resultado);
+    alert(`Tu IMC es ${imc.toFixed(2)}: ${mensaje}`);
+
+    
+    //Evento para ocultar las cartas informativas por defecto --> Corregir error! Se resetea y ejecuta y no se aprecia a leer la carta revelada
+    let cards = document.querySelectorAll('.result-card');
+    cards.forEach(card => card.style.display = "none");
+    
+    //Evento para mostrar la carta resultante -> Tras resultado hacer match y mostrar tarjeta
+    document.getElementById(resultadoId).style.display = "block";
+
 };
-
-//Evento para ocultar las cartas informativas por defecto
-// document.querySelectorAll('.card').forEach(function(card) {
-//     card.computedStyleMap.display = 'none';
-// });
-
-//Evento para mostrar la carta resultante -> Tras resultado hacer match y mostrar tarjeta
 
 //Convertir pulgadas a cm & viceversa -> Introducir numero / Seleccionar unidad / Seleccionar unidad conversion / Submit convertir
 
